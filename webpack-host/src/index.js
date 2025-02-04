82617d3b0fa5f6908a1f280341d4aadf6fc79b33
Stepan Svechnikov <dumn2011@gmail.com>
2025-02-04 20:32:13 +0300

    add: initial react app

---
+++ webpack-host/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./App";
+import "./App.scss";
+import {createRoot} from "react-dom/client";
+
+const el = document.getElementById("app");
+
+createRoot(el).render(<App/>);
