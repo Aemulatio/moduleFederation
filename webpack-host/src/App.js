58d303db9626c5b387cc4201ca3051403d855341
Stepan Svechnikov <dumn2011@gmail.com>
2025-02-04 20:37:25 +0300

    add: mini styles

--- webpack-host/src/App.js
+++ webpack-host/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+
 const App = () => {
-    return <h1>Hello React</h1>;
+    return <div className={'container'}>
+        <h1>Host App</h1>
+        <div>FOR SUB</div>
+
+    </div>;
 };
 
 export default App;
